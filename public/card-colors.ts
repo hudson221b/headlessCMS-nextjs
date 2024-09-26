7400da136bddb294d4b0814fdad80ac19d1eb38f
beizy <241zxy@gmail.com>
2024-09-26 11:29:35 -0400

    add colors file

---
+++ public/card-colors.ts
@@ -0,0 +1,15 @@
+/**
+ * Colors for unit cards' background gradient
+ */
+
+export const CARDCOLORS = [
+  ["#5027fc", "#8533c6"],
+  ["#8533c6", "#b03ed9"],
+  ["#b03ed9", "#de0075"],
+  ["#de0075", "#ff584a"],
+  ["#ff584a", "#ff9740"],
+  ["#6a9933", "#00a393"],
+  ["#00a393", "#007da7"],
+  ["#007da7", "#133e7a"],
+  ["#391ece", "#201a57"],
+]
